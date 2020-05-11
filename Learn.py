38066b99ec2a75fa5b12e085ee98d614b93bc907
zl18110 <1850541714@qq.com>
2020-05-11 11:08:23 +0800

    首次提交仓库信息

---
+++ Learn.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2020/5/7 16:14
+# @Author  : lary
+# @File    : Learn.py
+# print('hello world',type('hello world'))
+# print(1.0,type(1.0))
+# print(2+3,type(2+3))
+# a=10
+# b=20
+# if b>a:
+#     print('123')
+# else:
+#     print('456')
+

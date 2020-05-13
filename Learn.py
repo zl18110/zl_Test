3649717f7a03f33bb9e91c1f618f987a7b6ba7ce
zl18110 <1850541714@qq.com>
2020-05-13 09:47:26 +0800

    0947

--- Learn.py
+++ Learn.py
@@ -12,10 +12,32 @@
 #     print('123')
 # else:
 #     print('456')
-
-print('hello world')
-print(2+3)
-a=10
-print(a)
-b=8
-print(b)
+# print('hello world')
+# print(2+3)
+# a=10
+# print(a)
+# b=8
+# print(b)
+# print(type(1))
+# print(type(1.2))
+# print(type("hello world"))
+# print(isinstance(123,int))
+# print(isinstance(1.2,float))
+# print(isinstance('123',str))
+# print(0b101001) #二进制
+# print(0o101011) #十进制
+# print(0x101011) #十六进制
+#
+# print(123==123.0)
+# def add(a,b):
+#     c=a+b
+#     return c
+# c=add(4,5)
+# print(c)
+def my_abs(x):
+    if x>0:
+        return x
+    else:
+        return -x
+print(my_abs(99))
+print(my_abs(-99))

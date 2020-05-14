9f4542d7e11bf869a773a4c90fed9d7f2f6d2e71
zl18110 <1850541714@qq.com>
2020-05-14 17:09:07 +0800

    新增内容

---
+++ python learn/if else.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2020/5/14 9:47
+# @Author  : lary
+# @File    : if else.py
+import sys
+# age=int(input("请输入你的年龄："))
+# if age>18:
+#     print("可以观看")
+#     sys.exit()
+# else:
+#     print("不可以观看")
+#     sys.exit(
+# print("cc")#当前程序
+
+# a=int(input("输入高："))
+# b=int(input("输入宽："))
+# c=a+b
+# if c<15:
+#     print("c得到的数字大于15:",c)
+# elif c>=20 and c<25:
+#     print("c得到的数字大于20:",c)
+# elif c>=25 and c<36:
+#     print("c得到的数字大于25:",c)
+# else:
+#     print("C得到的数字小于:",c)
+# b=False
+# if b:
+#     print("b是true")
+# else:
+#     print("b是false")
+#
+# n=0
+# if n:
+#     print("n是零值")
+# else:
+#     print("n不是零值")
+
+# pr=int(input("请输入数字："))
+# if pr<10:
+#     print("显示不构成犯罪：",pr)
+# else:
+#     if pr >30 and pr<50:
+#         print("构成犯罪：",pr)
+#     else:
+#         print("已经达到犯罪：",pr)
+
+# pr=int(input("请输入数字："))
+# if pr<10:
+#     print("显示不构成犯罪：",pr)
+# else:
+#     if pr >30 and pr<50:
+#         print("构成犯罪：",pr)
+#     else:
+#         pass #跳出不执行
+
+math=int(input("请输入数字："))
+assert 0<=math<=100 #断言判断
+print("输入数字：",math)
---
+++ python learn/while.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2020/5/14 17:08
+# @Author  : lary
+# @File    : while.py
